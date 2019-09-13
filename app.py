3706655a493891d4a265e23f208e2a603e4e85ff
oskariwan <oskariwan40@gmail.com>
2019-09-13 09:51:15 +0000

    verkefni3

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 app = Flask(__name__)
 @app.route("/")
 def home():
@@ -8,7 +9,7 @@ def home():
 
 @app.route("/sida2/")
 def sida2():
-    return render_template("valmynd.html")+"""
+    return render_template("default.html")+"""
     <h1>Hello user2</h1>
     """
 @app.route("/sida3/")
@@ -16,6 +17,16 @@ def sida3():
     return render_template("valmynd.html")+"""
     <h1>Hello user3</h1>
     """
+@app.route("/sida/<kt>/")
+def page(kt):
+    summa=0
+    for item in kt:
+        summa = summa + int(item)
+    
+    return render_template("kt_summa.html", kt=kt, summa = summa)
 
+@app.errorhandler(404)
+def pagenotfound(error):
+    return render_template('pagenotfound.html'), 404
 if __name__ == "__main__":
     app.run(debug=True)
